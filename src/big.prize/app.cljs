1e246103ec835d539f8f2b8cc5b0fc7376bf3964
Samuel Schuhmacher <sammy.schuhmacher@gmail.com>
2014-01-25 17:16:37 +0100

    Add project skeleton. Fix a few basic issues.

--- src/big.prize/app.cljs
+++ src/big.prize/app.cljs
@@ -1,1 +1,13 @@
-(ns big.prize)
+(ns big.prize
+  (:require-macros [secretary.macros :refer [defroute]])
+  (:require [goog.events :as events]
+            [cljs.core.async :refer [put! <! chan]]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [secretary.core :as secretary])
+  (:import [goog History]
+           [goog.history EventType]))
+
+(enable-console-print!)
+
+(def app-state (atom {}))
