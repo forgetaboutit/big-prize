c7480ec2ef0f17bafec63ae4ee8bcb343a30b9d4
Samuel Schuhmacher <sammy.schuhmacher@gmail.com>
2014-01-26 23:39:09 +0100

    Add basic grid for categories and challenges.

--- src/big.prize/app.cljs
+++ src/big.prize/app.cljs
@@ -16,7 +16,12 @@
 (def app-state
   (atom
     {:teams []
-     :categories [{:name "" :challenges []}]}))
+     :categories [{:name "Allgemeinwissen"
+                   :challenges [{:text "Hallo Welt!" :taken false}
+                                {:text "Hello World!" :taken false}]}
+                  {:name "Sport"
+                   :challenges [{:text "Michael Schumacher" :taken false}
+                                {:text "Blarg!" :taken false}]}]}))
 
 (def team-id
   (let [id (atom 0)]
@@ -33,37 +38,68 @@
   (reify
     om/IRender
     (render [this]
-      (dom/li #js {:className "team"}
+      (dom/div #js {:className "team"}
         (dom/span nil (:points app))))))
 
 (defn teams-panel [app owner]
   (reify
     om/IRenderState
     (render-state [this {:keys [add-team]}]
-      (dom/div #js {:className "team-panel"}
+      (dom/div #js {:className "teams-panel"}
         (dom/button #js {:className "add-team"
                          :onClick #(put! add-team :add-team)}
           "+")
-        (dom/ul nil
-          (om/build-all team (:teams app)))))))
+        (om/build-all team (:teams app))))))
+
+(defn challenge [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [select-field]}]
+      (dom/div #js {:className "challenge"
+                    :onClick #(put! select-field @app)}
+        (:text app)))))
+
+(defn category [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [select-field]}]
+      (.log js/console app)
+      (dom/div #js {:className "category"}
+        (om/build-all challenge (:challenges app)
+          {:init-state {:select-field select-field}})))))
+
+(defn categories [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [select-field]}]
+      (dom/div #js {:className "categories"}
+        (om/build-all category (:categories app)
+          {:init-state {:select-field select-field}})))))
 
 (defn game [app owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:add-team (chan)})
+      {:add-team (chan)
+       :select-field (chan)})
     om/IWillMount
     (will-mount [_]
       (let [add-team (om/get-state owner :add-team)]
         (go (loop []
               (<! add-team)
               (om/transact! app :teams conj (create-team))
+              (recur))))
+      (let [select-field (om/get-state owner :select-field)]
+        (go (loop []
+              (.log js/console (<! select-field))
               (recur)))))
     om/IRenderState
-    (render-state [this {:keys [add-team]}]
-      (dom/div #js {:className "game"}
+    (render-state [this {:keys [add-team select-field]}]
+      (dom/section #js {:className "game"}
         (om/build teams-panel app
-          {:init-state {:add-team add-team}})))))
+          {:init-state {:add-team add-team}})
+        (om/build categories app
+          {:init-state {:select-field select-field}})))))
 
 (om/root
   app-state
