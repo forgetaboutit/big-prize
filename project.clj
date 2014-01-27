6ac1831191a22f35dab5b551624aba40014205d0
Samuel Schuhmacher <sammy.schuhmacher@gmail.com>
2014-01-27 21:42:43 +0100

    Restructure repository according to modern-cljs tutorial.

--- project.clj
+++ project.clj
@@ -8,17 +8,32 @@
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [secretary "0.4.0"]
-                 [om "0.3.0"]]
+                 [om "0.3.0"]
+                 [cljs-ajax "0.2.3"]
+                 [compojure "1.1.6"]
+                 [com.cemerick/piggieback "0.1.2"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.1"]
+            [lein-ring "0.8.8"]]
 
-  :source-paths ["src"]
+  :ring {:handler prize.core/handler}
+
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+
+  :injections [(require
+                 '[cljs.repl.browser :as brepl]
+                 '[cemerick.piggieback :as pb])
+               (defn browser-repl []
+                 (pb/cljs-repl :repl-env (brepl/repl-env :port 9000)))]
+
+  :source-paths ["src/clj" "src/cljs"]
 
   :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src"]
+    :builds [{
+              :source-paths ["src/cljs"]
               :compiler {
-                         :output-to "big-prize.js"
-                         :output-dir "out"
+                         :output-to "resources/public/js/big-prize.js"
+                         :output-dir "resources/public/js"
                          :optimizations :none
+                         :pretty-print true
                          :source-map true}}]})
---
+++ src/cljs/prize/core.cljs
@@ -0,0 +1,206 @@
+(ns big.prize
+  (:require-macros [cljs.core.async.macros :refer [go]]
+                   [secretary.macros :refer [defroute]])
+  (:require [goog.events :as events]
+            [cljs.core.async :refer [put! chan]]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [clojure.data :as data]
+            [clojure.string :as string]
+            [secretary.core :as secretary])
+  (:import [goog History]
+           [goog.history EventType]))
+
+(enable-console-print!)
+
+;; is-turn-of :team
+;;
+(def app-state
+  (atom
+    {:teams []
+     :categories [{:name "Allgemeinwissen"
+                   :challenges [{:text "Hallo Welt!" :taken false}
+                                {:text "Hello World!" :taken false}]}
+                  {:name "Sport"
+                   :challenges [{:text "Michael Schumacher" :taken false}
+                                {:text "Blarg!" :taken false}]}]}))
+
+(def team-id
+  (let [id (atom 0)]
+    (fn [] (do
+             (swap! id inc)
+             @id))))
+
+(defn create-team []
+  (let [id (team-id)]
+    (.log js/console (str id))
+    {:id id :points 0}))
+
+(defn team [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div #js {:className (str "team team-" (:id app))}
+        (dom/span nil (:points app))))))
+
+(defn teams-panel [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [add-team]}]
+      (dom/div #js {:className "teams-panel"}
+        (dom/button #js {:className "add-team"
+                         :onClick #(put! add-team :add-team)}
+          "+")
+        (om/build-all team (:teams app))))))
+
+(defn challenge [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [select-field]}]
+      (dom/div #js {:className "challenge"
+                    :onClick #(put! select-field @app)}
+        (:text app)))))
+
+(defn category [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [select-field]}]
+      (.log js/console app)
+      (dom/div #js {:className "category"}
+        (om/build-all challenge (:challenges app)
+          {:init-state {:select-field select-field}})))))
+
+(defn categories [app owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [select-field]}]
+      (dom/div #js {:className "categories"}
+        (om/build-all category (:categories app)
+          {:init-state {:select-field select-field}})))))
+
+(defn game [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:add-team (chan)
+       :select-field (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [add-team (om/get-state owner :add-team)]
+        (go (loop []
+              (<! add-team)
+              (om/transact! app :teams conj (create-team))
+              (recur))))
+      (let [select-field (om/get-state owner :select-field)]
+        (go (loop []
+              (.log js/console (<! select-field))
+              (recur)))))
+    om/IRenderState
+    (render-state [this {:keys [add-team select-field]}]
+      (dom/section #js {:className "game"}
+        (om/build teams-panel app
+          {:init-state {:add-team add-team}})
+        (om/build categories app
+          {:init-state {:select-field select-field}})))))
+
+;; ;;     om/IInitState
+;; ;;     (init-state [_]
+;; ;;       {:delete (chan)})
+;; ;;     om/IWillMount
+;; ;;     (will-mount [_]
+;; ;;       (let [delete (om/get-state owner :delete)]
+;; ;;         (go (loop []
+;; ;;               (let [contact (<! delete)]
+;; ;;                 (om/transact! app :contacts
+;; ;;                   (fn [xs] (into [] (remove #(= contact %) xs))))
+;; ;;                 (recur))))))
+;; ;;     om/IRenderState
+;; ;;     (render-state [this {:keys [delete]}]
+;; ;;       (dom/div nil
+;; ;;         (dom/h1 nil "Contact list")
+;; ;;         (apply dom/ul nil
+;; ;;           (om/build-all contact-view (:contacts app)
+;; ;;             {:init-state {:delete delete}}))
+;; ;;         (dom/div nil
+;; ;;           (dom/input #js {:type "text" :ref "new-contact"})
+;; ;;           (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
+
+;; (om/root
+;;   app-state
+;;   game
+;;   (.getElementById js/document "game-root"))
+
+;; ;; (def app-state
+;; ;;   (atom
+;; ;;     {:contacts
+;; ;;      [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+;; ;;       {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+;; ;;       {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+;; ;;       {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+;; ;;       {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+;; ;;       {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+;; ;; (defn middle-name [{:keys [middle middle-initial]}]
+;; ;;   (cond
+;; ;;     middle (str " " middle)
+;; ;;     middle-initial (str " " middle-initial ".")))
+
+;; ;; (defn display-name [{:keys [first last] :as contact}]
+;; ;;   (str last ", " first (middle-name contact)))
+
+;; ;; (defn parse-contact [contact-str]
+;; ;;   (let [[first middle last :as parts] (string/split contact-str #"\s+")
+;; ;;         [first last middle] (if (nil? last) [first middle] [first last middle])
+;; ;;         middle (when middle (string/replace middle "." ""))
+;; ;;         c (if middle (count middle) 0)]
+;; ;;     (when (>= (reduce + (map #(if % 1 0) parts)) 2)
+;; ;;       (cond-> {:first first :last last}
+;; ;;         (== c 1) (assoc :middle-initial middle)
+;; ;;         (>= c 2) (assoc :middle middle)))))
+
+;; ;; (defn add-contact [app owner]
+;; ;;   (let [input-field (om/get-node owner "new-contact")
+;; ;;         new-contact (-> input-field
+;; ;;                       .-value
+;; ;;                       parse-contact)]
+;; ;;     (when new-contact
+;; ;;       (om/transact! app :contacts conj new-contact)
+;; ;;       (set! (.-value input-field) ""))))
+
+;; ;; (defn contact-view [contact owner]
+;; ;;   (reify
+;; ;;     om/IRenderState
+;; ;;     (render-state [this {:keys [delete]}]
+;; ;;       (dom/li nil
+;; ;;         (dom/span nil (display-name contact))
+;; ;;         (dom/button #js {:onClick (fn [e] (put! delete @contact))}
+;; ;;           "Delete")))))
+
+;; ;; (defn contacts-view [app owner]
+;; ;;   (reify
+;; ;;     om/IInitState
+;; ;;     (init-state [_]
+;; ;;       {:delete (chan)})
+;; ;;     om/IWillMount
+;; ;;     (will-mount [_]
+;; ;;       (let [delete (om/get-state owner :delete)]
+;; ;;         (go (loop []
+;; ;;               (let [contact (<! delete)]
+;; ;;                 (om/transact! app :contacts
+;; ;;                   (fn [xs] (into [] (remove #(= contact %) xs))))
+;; ;;                 (recur))))))
+;; ;;     om/IRenderState
+;; ;;     (render-state [this {:keys [delete]}]
+;; ;;       (dom/div nil
+;; ;;         (dom/h1 nil "Contact list")
+;; ;;         (apply dom/ul nil
+;; ;;           (om/build-all contact-view (:contacts app)
+;; ;;             {:init-state {:delete delete}}))
+;; ;;         (dom/div nil
+;; ;;           (dom/input #js {:type "text" :ref "new-contact"})
+;; ;;           (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
+
+;; ;; (om/root
+;; ;;   app-state
+;; ;;   contacts-view
+;; ;;   (.getElementById js/document "game-root"))
